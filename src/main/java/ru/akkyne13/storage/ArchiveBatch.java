a2699e81e7b77848b2e0805eedcaab64db9c63f9
Gleb <zils.gleb@gmail.com>
2026-09-22 21:25:43 +0700

    feat: added ArchiveBatch class isolated from other Batch classes

---
+++ src/main/java/ru/akkyne13/storage/ArchiveBatch.java
@@ -0,0 +1,12 @@
+package ru.akkyne13.storage;
+
+import java.time.LocalDate;
+
+public record ArchiveBatch(
+        String sku,
+        String name,
+        int amount,
+        String cell,
+        LocalDate receiptDate,
+        LocalDate archiveDate,
+        String archiveReason) { }
