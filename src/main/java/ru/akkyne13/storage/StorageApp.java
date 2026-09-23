fceea1f7923e558531772d6ce10ee0e788841dc2
Gleb <zils.gleb@gmail.com>
2026-09-24 00:38:51 +0700

    feat: added columns for archive batches, changed buttons alignment, added "edit" button disable state

--- src/main/java/ru/akkyne13/storage/StorageApp.java
+++ src/main/java/ru/akkyne13/storage/StorageApp.java
@@ -3,6 +3,7 @@ package ru.akkyne13.storage;
 import javafx.application.Application;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
@@ -29,13 +30,23 @@ public class StorageApp extends Application {
         importBatches.add(new ImportBatch("ОЗН501256023", "Попрыгунчик", 300, "CY1725",
                 LocalDate.of(2026, 4, 7), "China", "HZ322228"));
 
+        List<ArchiveBatch> archiveBatches = new ArrayList<>();
+        archiveBatches.add(new ArchiveBatch("ОЗН234525472", "Чайник", 40, "AH2501",
+                LocalDate.of(2026, 3, 16), LocalDate.of(2026, 9, 22),
+                "Закончились товары"));
+
 
         VBox root = new VBox();
-        HBox buttonsBox = new HBox();
+        HBox topButtonsBox = new HBox();
+        HBox buttomButtonsBox = new HBox();
 
         Button loadFromCsv = new Button("Загрузить из файла");
         Button saveToCsv = new Button("Сохранить в файл");
+
+
         Button editRecord = new Button("Изменить запись");
+        editRecord.setDisable(true);
+
         Button addRecord = new Button("Добавить запись");
 
         TableView<StorageItem> recordsTable = new TableView<>();
@@ -46,13 +57,13 @@ public class StorageApp extends Application {
         TableColumn<StorageItem, String> nameCol = new TableColumn<>("Наименование");
         nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
 
-        TableColumn<StorageItem, String> amountCol = new TableColumn<>("Количество");
+        TableColumn<StorageItem, Integer> amountCol = new TableColumn<>("Количество");
         amountCol.setCellValueFactory(new PropertyValueFactory<>("amount"));
 
         TableColumn<StorageItem, String> cellCol = new TableColumn<>("Ячейка");
         cellCol.setCellValueFactory(new PropertyValueFactory<>("cell"));
 
-        TableColumn<StorageItem, String> receiptDateCol = new TableColumn<>("Дата поступления");
+        TableColumn<StorageItem, LocalDate> receiptDateCol = new TableColumn<>("Дата поступления");
         receiptDateCol.setCellValueFactory(new PropertyValueFactory<>("receiptDate"));
 
         TableColumn<StorageItem, String> countryCol = new TableColumn("Страна");
@@ -75,17 +86,59 @@ public class StorageApp extends Application {
             return new SimpleStringProperty("-");
         });
 
-        recordsTable.getColumns().addAll(skuCol, nameCol, amountCol, cellCol, receiptDateCol, countryCol, customsCodeCol);
+        TableColumn<StorageItem, LocalDate> archiveDateCol = new TableColumn("Дата архивации");
+        archiveDateCol.setCellValueFactory(cellData -> {
+            StorageItem item = cellData.getValue();
+            if (item instanceof ArchiveBatch archiveBatch) {
+                return new SimpleObjectProperty(archiveBatch.archiveDate());
+            }
+
+            return new SimpleObjectProperty("-");
+        });
+
+        TableColumn<StorageItem, String> archiveReasonCol = new TableColumn("Причина архивации");
+        archiveReasonCol.setCellValueFactory(cellData -> {
+            StorageItem item = cellData.getValue();
+            if (item instanceof ArchiveBatch archiveBatch) {
+                return new SimpleStringProperty(archiveBatch.archiveReason());
+            }
+
+            return new SimpleStringProperty("-");
+        });
+
+        recordsTable.getColumns().addAll(skuCol, nameCol, amountCol,
+                cellCol, receiptDateCol, countryCol,
+                customsCodeCol, archiveDateCol, archiveReasonCol);
+
         recordsTable.getItems().addAll(batches);
         recordsTable.getItems().addAll(importBatches);
+        recordsTable.getItems().addAll(archiveBatches);
 
 
-        buttonsBox.getChildren().addAll(loadFromCsv, saveToCsv, editRecord, addRecord);
-        buttonsBox.setSpacing(10);
+        topButtonsBox.getChildren().addAll(loadFromCsv, saveToCsv);
+        topButtonsBox.setSpacing(10);
 
-        root.getChildren().addAll(buttonsBox, recordsTable);
+        buttomButtonsBox.getChildren().addAll(editRecord, addRecord);
+        buttomButtonsBox.setSpacing(10);
+        buttomButtonsBox.setAlignment(Pos.CENTER_RIGHT);
+
+        root.getChildren().addAll(topButtonsBox, recordsTable, buttomButtonsBox);
         root.setPadding(new Insets(10));
         root.setSpacing(10);
+
+
+
+        recordsTable.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> {
+                    if (newValue == null) {
+                        editRecord.setDisable(true);
+                    } else {
+                        editRecord.setDisable(!(newValue instanceof Editable));
+                    }
+                }
+        );
+
+        // TODO: написать setOnAction для каждой кнпки (для начала - тестовый)
 
         VBox.setVgrow(recordsTable, Priority.ALWAYS);
 
