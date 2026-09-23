f879e15baaeb75ef0352c3074ad29ce07dec82a4
Gleb <zils.gleb@gmail.com>
2026-09-24 00:39:34 +0700

    refactor: test archive batch moved to GUI controller (temporarily)

--- src/main/java/ru/akkyne13/storage/Main.java
+++ src/main/java/ru/akkyne13/storage/Main.java
@@ -6,18 +6,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-        List<ArchiveBatch> archiveBatches = new ArrayList<>();
-        archiveBatches.add(new ArchiveBatch("ОЗН234525472", "Чайник", 40, "AH2501",
-                LocalDate.of(2026, 3, 16), LocalDate.of(2026, 9, 22),
-                "Закончились товары"));
-
-        IO.println();
-
-        for (ArchiveBatch batch : archiveBatches) {
-            IO.println(batch);
-        }
-
         StorageApp.main(args);
     }
 }
